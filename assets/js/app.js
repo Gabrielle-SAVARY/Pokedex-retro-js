5288781c599d44b683b68349c9e042bb18940a96
Gabrielle-SAVARY <savary.gabrielle@hotmail.fr>
2023-09-04 22:53:02 +0200

    Add feature: pokemons types filters buttons

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
-const pokedexUrl = './data/pokedex.json';
 let pokedex = [];
+let pokemonsTypes = [];
 let currentPage = 1;
 const pokedexEnd = 151;
 const pokemonPerPage = 20;
@@ -9,7 +9,17 @@ const searchInput = document.getElementById('search-input');
 // Chargement du fichier JSON du pokedex
 async function getPokedex() {
 	try {
-		const response = await fetch(pokedexUrl);
+		const response = await fetch('./data/pokedex.json');
+		const data = await response.json();
+		return data;
+	} catch (error) {
+		console.error('Erreur lors du chargement des données:', error);
+	}
+}
+// Chargement du fichier JSON du pokedex
+async function getPokemonsTypes() {
+	try {
+		const response = await fetch('./data/pokemonsTypes.json');
 		const data = await response.json();
 		return data;
 	} catch (error) {
@@ -17,32 +27,32 @@ async function getPokedex() {
 	}
 }
 
-// Fonction pour afficher les Pokémons dans le DOM
-function displayPokemon(pokemonList,pokemonStart, pokemonMax) {
+// Affiche les Pokémons dans le DOM
+function displayPokemon(pokemonList) {
 	console.log('pokemonList', pokemonList);
 	// Récupérez le conteneur principal où vous voulez afficher les cartes Pokémon
 	const pokemonsList = document.getElementById('pokemon-list');
 
-	// Boucle à travers les 20 premiers Pokémon 
-	for (let i = pokemonStart; i < pokemonMax; i++) {
-		// Obtenez le Pokémon actuel depuis l'objet pokedex.
-		const pokemon = pokedex[i]; 
+	// Boucle sur les pokemons du pokedex
+	for (let i = 0; i < pokedexEnd; i++) {
+		// Pokémon actuel 
+		const pokemon = pokedex[i];
 
 		// Créez une div pour la carte Pokémon
 		const pokemonCard = document.createElement('article');
-		pokemonCard.classList.add('pokemon-card'); 
+		pokemonCard.classList.add('pokemon-card');
 
 		// Créez une div pour les informations du Pokémon
 		const pokemonInfos = document.createElement('div');
-		pokemonInfos.classList.add('pokemon-infos'); 
+		pokemonInfos.classList.add('pokemon-infos');
 		// Créez un élément pour afficher l'identification du Pokémon
 		const pokemonIdentification = document.createElement('h2');
-		pokemonIdentification.textContent =  `# ${pokemon.index} ${pokemon.name}`;
+		pokemonIdentification.textContent = `# ${pokemon.index} ${pokemon.name}`;
 		pokemonIdentification.classList.add('pokemon-identification');
 		// Créez un élément pour afficher le type du Pokémon
 		const pokemonType = document.createElement('h3');
 		pokemonType.textContent = pokemon.type;
-		pokemonType.classList.add('pokemon-type'); 
+		pokemonType.classList.add('pokemon-type');
 		// Ajoute le nom et le type du Pokémon à la carte Pokémon
 		pokemonInfos.appendChild(pokemonIdentification);
 		pokemonInfos.appendChild(pokemonType);
@@ -50,8 +60,8 @@ function displayPokemon(pokemonList,pokemonStart, pokemonMax) {
 		// Créez un élément image pour afficher l'image du Pokémon
 		const pokemonImage = document.createElement('img');
 		pokemonImage.src = pokemon.image;
-		pokemonImage.alt = pokemon.name; 
-		pokemonImage.classList.add('pokemon-img'); 
+		pokemonImage.alt = pokemon.name;
+		pokemonImage.classList.add('pokemon-img');
 
 		// Ajoute les informations à la carte Pokémon
 		pokemonCard.appendChild(pokemonImage);
@@ -61,28 +71,79 @@ function displayPokemon(pokemonList,pokemonStart, pokemonMax) {
 	}
 }
 
-// Fonction asynchrone pour afficher les 20 premiers Pokémon au chargerment de la page
+
+function filterPokemonByType(type) {
+	// Sélectionne tous les éléments Pokémon
+	const pokemonCards = document.querySelectorAll('.pokemon-card');
+
+	// Parcoure tous les éléments Pokémon pour les afficher ou les masquer en fonction du type
+	pokemonCards.forEach(pokemonCard => {
+		// Récupère le type du Pokémon 
+		const pokemonType = pokemonCard.querySelector('.pokemon-type').textContent;
+		// Vérifie si le type du Pokémon est inclus dans le type sélectionné
+		if (pokemonType.includes(type)) {
+			// Si le type correspond, affiche le Pokémon
+			pokemonCard.style.display = 'flex';
+		} else {
+			// Sinon, masquez le Pokémon 
+			pokemonCard.style.display = 'none';
+		}
+	});
+}
+
+
+// Créer les boutons de filtre de type de Pokémon
+function createTypeFilterButtons(types) {
+	const typesList = document.querySelector('.types-list');
+
+	types.forEach(type => {
+		const button = document.createElement('button');
+		button.textContent = type.typeName;
+		button.classList.add('type-filter-button');
+		// Ajoute un style de couleur au bouton 
+		button.style.backgroundColor = type.typeColor;
+		// Ajoute un écouteur d'événement pour le clic sur le bouton
+		button.addEventListener('click', function () {
+			// Supprimez la classe active de tous les boutons
+      document.querySelectorAll('.type-filter-button').forEach(btn => {
+        btn.classList.remove('active');
+      });
+
+      // Ajoutez la classe active au bouton actuellement sélectionné
+      button.classList.add('active');
+			filterPokemonByType(type.typeName);
+		});
+		// Ajoute le bouton au conteneur des boutons de filtre
+		typesList.appendChild(button);
+	});
+}
+
+// Affiche les données au chargerment de la page
 async function loadPokedex() {
 	pokedex = await getPokedex();
+	pokemonsTypes = await getPokemonsTypes();
 	console.log('pokedex', pokedex);
-	displayPokemon(pokedex,0, 151);
+	console.log('pokemonsTypes', pokemonsTypes);
+	displayPokemon(pokedex);
+	createTypeFilterButtons(pokemonsTypes);
 }
 
 // Barre de recherche - effectue la recherche et affiche les cartes Pokémon correspondantes
 function handleSearch() {
-  const searchTerm = searchInput.value.toLowerCase();
-  const pokemonCards = document.querySelectorAll('.pokemon-card');
-
-  pokemonCards.forEach(pokemonCard => {
-    const pokemonIdentity = pokemonCard.querySelector('.pokemon-identification').textContent.toLowerCase();
-    const pokemonType = pokemonCard.querySelector('.pokemon-type').textContent.toLowerCase();
-
-    if (pokemonIdentity.includes(searchTerm) || pokemonType.includes(searchTerm)) {
-      pokemonCard.style.display = 'flex';
-    } else {
-      pokemonCard.style.display = 'none';
-    }
-  });
+	// Recherche de l'utilisateur
+	const searchTerm = searchInput.value.toLowerCase();
+	// Sélection de tous les Pokémon
+	const pokemonCards = document.querySelectorAll('.pokemon-card');
+	// Recherche
+	pokemonCards.forEach(pokemonCard => {
+		const pokemonIdentity = pokemonCard.querySelector('.pokemon-identification').textContent.toLowerCase();
+		const pokemonType = pokemonCard.querySelector('.pokemon-type').textContent.toLowerCase();
+		if (pokemonIdentity.includes(searchTerm) || pokemonType.includes(searchTerm)) {
+			pokemonCard.style.display = 'flex';
+		} else {
+			pokemonCard.style.display = 'none';
+		}
+	});
 }
 
 // Chargement du pokedex sur la page d'accueil
@@ -90,6 +151,33 @@ loadPokedex();
 
 // Ecouteur d'évenement sur l'input de la barre de recherche
 searchInput.addEventListener('input', handleSearch);
+
+
+
+
+// Sélection du bouton de réinitialisation des filtres de type
+const resetFilterButton = document.getElementById('reset-filter-button');
+
+// Ajoutez un gestionnaire d'événement pour le clic sur le bouton de réinitialisation
+resetFilterButton.addEventListener('click', function () {
+	// Appelez une fonction pour réinitialiser le filtre
+	resetFilter();
+});
+
+function resetFilter() {
+	const pokemonCards = document.querySelectorAll('.pokemon-card');
+	// Affiche tous les Pokémon
+	pokemonCards.forEach(pokemonCard => {
+		pokemonCard.style.display = 'flex';
+	});
+	// Supprime la classe active de tous les boutons
+	document.querySelectorAll('.type-filter-button').forEach(btn => {
+		btn.classList.remove('active');
+	});
+}
+
+
+
 
 
 
